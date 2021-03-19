14184816a27900d7317aa9553c4548ecc51c270c
taosue <taosue314@gmail.com>
2021-03-20 03:08:33 +0800

    first commit

---
+++ docusaurus.config.js
@@ -0,0 +1,82 @@
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'DragonGuild',
+  tagline: 'Fight for Web3',
+  url: 'https://dragonguild.org',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'dragonguild', // Usually your GitHub org/user name.
+  projectName: 'dragonguild-web', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'DragonGuild',
+      logo: {
+        alt: 'DragonGuild Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Getting Started',
+              to: 'docs/',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Discord',
+              href: 'https://discord.gg/xwUUXQV',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/DragonGuildDAO',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://github.com/dragonguild',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} DragonGuild,DAO.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
